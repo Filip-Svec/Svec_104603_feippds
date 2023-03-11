3d3cad00403d7edd0030008cb7fa678452e2f084
Filip Svec <xsvecf@stuba.sk>
2023-03-11 17:29:25 +0100

    feat: initialize file

---
+++ diningPhilosophers.py
@@ -0,0 +1,11 @@
+"""This module implements dinning philosophers problem.
+
+
+"""
+
+__author__ = "Filip Švec, Tomáš Vavro"
+__email__ = "xsvecf@stuba.sk, xvavro@stuba.sk"
+__license__ = "MIT"
+
+from fei.ppds import Thread, Mutex, Semaphore, print
+from time import sleep
