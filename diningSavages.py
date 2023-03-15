0a5504ecbd75c4235c58579bebb412300fffb2ff
Filip Svec <xsvecf@stuba.sk>
2023-03-15 15:33:32 +0100

    docs: initialize file with header

---
+++ diningSavages.py
@@ -0,0 +1,15 @@
+"""This module implements dinning savages problem.
+
+This implementation of the dining savages problem
+utilizes more than one cook.
+University: STU Slovak Technical University in Bratislava
+Faculty: FEI Faculty of Electrical Engineering and Information Technology
+Year: 2023
+"""
+
+__author__ = "Filip Švec, Tomáš Vavro, Marián Šebeňa"
+__email__ = "xsvecf@stuba.sk, xvavro@stuba.sk, mariansebena@stuba.sk"
+__license__ = "MIT"
+
+from fei.ppds import Thread, Mutex, print
+from time import sleep
