a5a46273cbdbc09c5a475ceba345d2243f27976a
Filip Svec <xsvecf@stuba.sk>
2023-05-12 20:12:25 +0200

    docs: add documentation

--- cudaGrayScale.py
+++ cudaGrayScale.py
@@ -1,7 +1,7 @@
-"""This module implements dinning savages problem.
+"""This module implements image converting function to greyscale.
 
-This implementation of the dining savages problem
-utilizes more than one cook.
+This implementation uses numba library to parallelize computation
+of conversion of an image to greyscale.
 University: STU Slovak Technical University in Bratislava
 Faculty: FEI Faculty of Electrical Engineering and Information Technology
 Year: 2023
@@ -17,8 +17,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+
 def grayscale_CPU(image):
-    """Convert an RGB image to grayscale.
+    """Convert an RGB image to grayscale with CPU.
 
     Args:
         image - image data
@@ -31,6 +32,13 @@ def grayscale_CPU(image):
 
 @cuda.jit
 def grayscale_kernel(image, gray):
+    """A CUDA kernel that converts a 3-channel RGB image to grayscale.
+
+        Args:
+            image: the input image.
+            gray: the output grayscale image.
+    """
+
     row, col = cuda.grid(2)  # Get the thread's row and column indices
     if row < image.shape[0] and col < image.shape[1]:
         r, g, b = image[row, col]
@@ -38,7 +46,14 @@ def grayscale_kernel(image, gray):
 
 
 def grayscale(image):
-    """Convert an image to grayscale using CUDA."""
+    """Convert an image to grayscale using CUDA.
+
+        Args:
+            image - image data
+
+        Returns:
+            output_image
+    """
 
     # Get the dimensions of the input image
     height, width, channels = image.shape
@@ -68,34 +83,22 @@ def main():
     directory = "imgz/"
     files = os.listdir(directory)
 
-    index = 0
+    # Loop through all files using GPU
     for filename in files:
         print(filename)
-
         start_time = time.time()
         image = plt.imread("imgz/" + filename)
         newImage = grayscale(image)
         print("--- %s seconds ---" % (time.time() - start_time))
-
         plt.imsave("imgz_grey/" + "grey_" + filename, newImage, cmap='gray', format="jpg")
 
-        # index = index + 1
-        # # print(index)
-        # if index >= 5:
-        #     break
-
+    # Loop through all files using CPU
     for filename in files:
         print(filename)
-
         start_time = time.time()
         image = plt.imread("imgz/" + filename)
         grayscale_CPU(image)
         print("--- %s seconds ---" % (time.time() - start_time))
-
-        # index = index + 1
-        # print(index)
-        # if index >= 5:
-        #     break
 
 
 if __name__ == "__main__":
